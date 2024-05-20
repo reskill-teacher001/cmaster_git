c94baf3998c3d99b5d5de6d74a98c28bf1f70a2a
student <student@WIN-H7M01J10L01>
2024-05-20 09:55:39 +0900

    JM21 Step3

--- src/main/java/kadai21/BbsServlet2.java
+++ src/main/java/kadai21/BbsServlet2.java
@@ -1,10 +1,10 @@
 package kadai21;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -50,36 +50,39 @@ public class BbsServlet2 extends HttpServlet {
 		
 		list.add(name + "：" + message);
 		
-		//Webブラウザへのお知らせ情報の設定
-		response.setContentType("text/html; charset=UTF-8");
+		RequestDispatcher rd = request.getRequestDispatcher("/ShowServlet");
+		rd.forward(request, response);
 		
-		PrintWriter out = response.getWriter();
-		
-		out.println("<!DOCTYPE html>");
-		out.println("<html>");
-		out.println("<head>");
-		out.println("<title>掲示板</title>");
-		out.println("</head>");
-		out.println("<body>");
-		
-		out.println("<form action=\"/cmaster/BbsServlet2\" method=\"post\">");
-		out.println("名前：<br>");
-		out.println("<input type=\"text\" name=\"NAME\">");
-		out.println("<br>");
-		out.println("メッセージ：<br>");
-		out.println("<textarea name=\"MESSAGE\" cols=\"30\" rows=\"5\"></textarea>");
-		out.println("<br>");
-		out.println("<input type=\"submit\" value=\"書き込み\">");
-		out.println("</form>");
-		out.println("<hr>");
-	
-		for (String msg : list) {
-			out.println(msg);
-			out.println("<hr>");
-		}
-		
-		out.println("</body>");
-		out.println("</html>");
+//		//Webブラウザへのお知らせ情報の設定
+//		response.setContentType("text/html; charset=UTF-8");
+//		
+//		PrintWriter out = response.getWriter();
+//		
+//		out.println("<!DOCTYPE html>");
+//		out.println("<html>");
+//		out.println("<head>");
+//		out.println("<title>掲示板</title>");
+//		out.println("</head>");
+//		out.println("<body>");
+//		
+//		out.println("<form action=\"/cmaster/BbsServlet2\" method=\"post\">");
+//		out.println("名前：<br>");
+//		out.println("<input type=\"text\" name=\"NAME\">");
+//		out.println("<br>");
+//		out.println("メッセージ：<br>");
+//		out.println("<textarea name=\"MESSAGE\" cols=\"30\" rows=\"5\"></textarea>");
+//		out.println("<br>");
+//		out.println("<input type=\"submit\" value=\"書き込み\">");
+//		out.println("</form>");
+//		out.println("<hr>");
+//	
+//		for (String msg : list) {
+//			out.println(msg);
+//			out.println("<hr>");
+//		}
+//		
+//		out.println("</body>");
+//		out.println("</html>");
 	}
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
